38569874629357e14a6090e94c1c78e582a69a0e
Allison DiNapoli <adinapol@asu.edu>
2019-11-21 22:34:48 -0800

    Updates

---
+++ precache-manifest.6a08b50865b4f4a6bc1d0ceea0396cac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df40c7645754959074e2891da7e555d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a9666783ca9f0429f0",
+    "url": "/static/css/main.88f61a1f.chunk.css"
+  },
+  {
+    "revision": "c7546ce59dc678a5187e",
+    "url": "/static/js/2.5549297c.chunk.js"
+  },
+  {
+    "revision": "d5a9666783ca9f0429f0",
+    "url": "/static/js/main.84eb354b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "249147d391fd58c876e25baa7ac97c67",
+    "url": "/static/media/GeosansLight.249147d3.eot"
+  },
+  {
+    "revision": "36e6a06a4146af2f966ffcf021751724",
+    "url": "/static/media/GeosansLight.36e6a06a.ttf"
+  }
+]);
