93da65902aceeb81e4a1b395eaaef45b429c3a77
Allison DiNapoli <adinapol@asu.edu>
2020-01-12 10:00:49 -0800

    a few updates and updating CNAME to be papaysolomon.com

--- src/components/news.js
+++ src/components/news.js
@@ -21,11 +21,26 @@ export default class News extends React.Component {
             <div className="newsHeader">AFRICAN FOR THE FIRST TIME</div> 
             <div className="newsText">Solo Exhibition</div>
             <div className="newsText">Joseph Gross Gallery, University of Arizona</div>
-            <div className="newsText">January 30th - March 6th</div>
-            <div className="newsText">Opening: January 30th from 5:00pm - 6:30pm</div>
+            <div className="newsText">January 30th - March 10th</div>
+            <div className="newsText">Artist Talk: Febuary 13th from 4:00pm - 5:00pm</div>
+            <div className="newsText">Reception: Febuary 13th from 5:00pm - 6:30pm</div>
           </div> 
         </div>
 
+        
+
+        <div style={{paddingTop: "5vh"}}> </div>
+        <div className="vimeoVideo">
+          <iframe src="https://player.vimeo.com/video/361556387" 
+            style={{position: "absolute", top:0, left:0, width: '100%', height:'100%'}}
+            frameborder="0" 
+            allow="autoplay; fullscreen" 
+            webkitallowfullscreen 
+            mozallowfullscreen 
+            allowfullscreen>
+          </iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
         <br/>
         <div className="vimeoVideo">
           <iframe title="Video: ASU Senior Awarded for Life-like Portraits of African Refugees - 3TV/CBS 5 News"
@@ -36,18 +51,6 @@ export default class News extends React.Component {
                   webkitallowfullscreen 
                   mozallowfullscreen 
                   allowfullscreen>
-          </iframe>
-        </div>
-        <script src="https://player.vimeo.com/api/player.js"></script>
-        <div style={{paddingTop: "5vh"}}> </div>
-        <div className="vimeoVideo">
-          <iframe src="https://player.vimeo.com/video/361556387" 
-            style={{position: "absolute", top:0, left:0, width: '100%', height:'100%'}}
-            frameborder="0" 
-            allow="autoplay; fullscreen" 
-            webkitallowfullscreen 
-            mozallowfullscreen 
-            allowfullscreen>
           </iframe>
         </div>
         <script src="https://player.vimeo.com/api/player.js"></script>
