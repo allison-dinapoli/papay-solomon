f00da4ac4254e39967d37f01722903fb6fcca2a7
Allison DiNapoli <adinapol@asu.edu>
2020-01-24 20:00:16 -0800

    Updates

--- precache-manifest.18883d0d39853e0664fec147c413bc68.js
+++ precache-manifest.18883d0d39853e0664fec147c413bc68.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ec6aec615f838342de969254829c5c8",
+    "revision": "677611e0db644c8acae165ed36ab9bd8",
     "url": "/index.html"
   },
   {
-    "revision": "550aad48ec7326a8b23a",
-    "url": "/static/css/main.38f05a4c.chunk.css"
+    "revision": "8393e6b235e8f94ff2e7",
+    "url": "/static/css/main.01cb54de.chunk.css"
   },
   {
     "revision": "88b354d2a31a20e07853",
     "url": "/static/js/2.6f9b4951.chunk.js"
   },
   {
-    "revision": "550aad48ec7326a8b23a",
+    "revision": "8393e6b235e8f94ff2e7",
     "url": "/static/js/main.a9ee881b.chunk.js"
   },
   {
