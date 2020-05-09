cba2fa774d7663fd274dd2d7a1dc1cb261afa403
Allison DiNapoli <adinapol@asu.edu>
2020-05-09 14:34:29 -0700

    Updates

--- precache-manifest.1f10cbf079d0f330b0e56613dfbf920e.js
+++ precache-manifest.1f10cbf079d0f330b0e56613dfbf920e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7a76e27fc5efc4c50abf3ccf28f259f",
+    "revision": "1b9ed3f20bd54cb9d71dd97366fafef3",
     "url": "/index.html"
   },
   {
-    "revision": "5739780e2d6f27748325",
-    "url": "/static/css/main.20deba61.chunk.css"
+    "revision": "c39fca3a59a0441a4d4b",
+    "url": "/static/css/main.021fc5d5.chunk.css"
   },
   {
-    "revision": "120aa7786694d584d737",
-    "url": "/static/js/2.1422cc9c.chunk.js"
+    "revision": "282ee0e5e25768ed0cc0",
+    "url": "/static/js/2.03af9e8e.chunk.js"
   },
   {
     "revision": "b98548b764a59ce55bce02da3d8020d8",
-    "url": "/static/js/2.1422cc9c.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.03af9e8e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5739780e2d6f27748325",
-    "url": "/static/js/main.34689f42.chunk.js"
+    "revision": "c39fca3a59a0441a4d4b",
+    "url": "/static/js/main.9a0819c3.chunk.js"
   },
   {
     "revision": "6036d4821f66a97c2e0b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdd6c1c201fd98cc5b0c85a1fe258017.js"
+  "/precache-manifest.1f10cbf079d0f330b0e56613dfbf920e.js"
 );
 
 self.addEventListener('message', (event) => {
